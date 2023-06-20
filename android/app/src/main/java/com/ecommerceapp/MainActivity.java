1d0d6b52e5d1b1ac3b34a2617e915842b88a1dd3
Shubhi Singh <shubhisingh1506@gmail.com>
2023-06-20 17:06:12 +0530

    Project structure and basic setup

--- android/app/src/main/java/com/ecommerceapp/MainActivity.java
+++ android/app/src/main/java/com/ecommerceapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ecommerceapp;
+import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +18,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "EcommerceApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
